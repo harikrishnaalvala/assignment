64cce0a8c8606e3ed35b5e25a63bbb88881dcf7a
harikrishna <harikrishnaalvala@gmail.com>
2024-04-10 10:41:24 +0000

    solution

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import './App.css'
-import {Route, BrowserRouter, Routes} from 'react-router-dom'
 import Home from './components/Home'
-import TopRated from './components/TopRated'
-import UpComing from './components/UpComing'
-// import SinglePage from "./components/MovieDetails";
-import Navbar from './components/Navbar'
 import MovieDetails from './components/MovieDetails'
-import NotFound from './components/NotFound'
-import SearchPage from './components/SearchPage'
-const App = () => {
-  return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/top-rated" element={<TopRated />} />
-        <Route path="/up-coming" element={<UpComing />} />
-        <Route path="/movie/:id" element={<MovieDetails />} />
-        <Route path="/search/:searchTerm" element={<SearchPage />} />
-        <Route path="/movie/*" element={<NotFound />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
-  )
-}
+import TopRatedMovies from './components/TopRatedMovies'
+import UpcomingMovies from './components/UpcomingMovies'
+import SearchResultsPage from './components/SearchResultsPage'
+
+const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route exact path="/movie_details/:id" element={<MovieDetails />} />
+      <Route exact path="/top_rated_movies" element={<TopRatedMovies />} />
+      <Route exact path="/upcoming_movies" element={<UpcomingMovies />} />
+      <Route exact path="/search_results" element={<SearchResultsPage />} />
+    </Routes>
+  </BrowserRouter>
+)
+
 export default App
